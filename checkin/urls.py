c92dc44d8e4d38d0cdd8f647e60c6217e0e973a7
tas12740 <tas12740@gmail.com>
2019-08-28 14:17:19 -0400

    Sibling registration functionality

--- checkin/urls.py
+++ checkin/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('<str:unique_id>/', views.checkin, name='checkin'),
     path('api/findEvent/', views.find_event, name='findEvent'),
-    path('api/checkin/', views.add_checkin, name='handleCheckIn')
+    path('api/checkin/', views.add_checkin, name='handleCheckIn'),
+    path('register/sibling/', views.register_sibling, name='registerSibling')
 ]
--- checkin/views.py
+++ checkin/views.py
@@ -71,3 +71,53 @@ def add_checkin(request):
     new_checkin.save()
 
     return HttpResponse(status=201)
+
+
+def register_sibling(request):
+    if request.method == 'POST':
+        data = request.POST
+
+        onyen = data.get('onyen')
+
+        if onyen is None:
+            return HttpResponseBadRequest('You must submit your onyen.')
+
+        if Sibling.objects.filter(onyen=onyen).exists():
+            return HttpResponseBadRequest(f'A sibling with the onyen {onyen} already exists.')
+
+        first_name = data.get('first_name')
+
+        if first_name is None:
+            return HttpResponseBadRequest('You must submit your first name')
+
+        first_name = first_name.title()
+
+        last_name = data.get('last_name')
+
+        if last_name is None:
+            return HttpResponseBadRequest('You must submit your last name.')
+
+        last_name = last_name.title()
+
+        email = data.get('email')
+
+        if email is None:
+            return HttpResponseBadRequest('You must submit your email.')
+
+        pledge_class = data.get('pledge_class')
+
+        if pledge_class is None:
+            return HttpResponseBadRequest('You must submit your pledge class.')
+
+        pledge_class = pledge_class.title()
+
+        new_sibling = Sibling(first_name=first_name, last_name=last_name,
+                              onyen=onyen, email=email, pledge_class=pledge_class)
+        try:
+            new_sibling.save()
+        except Exception as e:
+            return HttpResponseBadRequest(f'{e}')
+
+        return HttpResponse(status=201)
+
+    return render(request, 'checkin/register.html')

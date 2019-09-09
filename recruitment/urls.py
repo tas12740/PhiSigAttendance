816d44729b39d2a66d87f883b3d9107ddec05c58
tas12740 <tas12740@gmail.com>
2019-09-09 15:24:32 -0400

    Adding database to the git repository

--- recruitment/urls.py
+++ recruitment/urls.py
@@ -5,5 +5,6 @@ from . import views
 app_name = 'recruitment'
 
 urlpatterns = [
-    path('checkin/', views.checkin, name='checkin')
+    path('checkin/', views.checkin, name='checkin'),
+    path('emails/', views.emails, name='emails')
 ]
--- recruitment/views.py
+++ recruitment/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+from .models import PNM
 
 # Create your views here.
+
+
 def checkin(request):
     return render(request, 'recruitment/checkin.html')
+
+
+def emails(request):
+    pnms = PNM.objects.all()
+
+    emails = []
+    for new_pnm in pnms:
+        emails.append(new_pnm.email)
+
+    return HttpResponse(','.join(emails))

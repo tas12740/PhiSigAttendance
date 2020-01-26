271e656b4848e275b7ae2bc03b262b45fcead062
tas12740 <tas12740@gmail.com>
2020-01-26 17:58:51 -0500

    Auto resizing of carousel based on screen width

--- static/root/js/index.js
+++ static/root/js/index.js
@@ -3,4 +3,19 @@ $(document).ready(function () {
         ride: true,
         pause: false
     });
+
+    const width = $('.carousel-img').width();
+    // 13/5 = curr_width/curr_height => curr_height = curr_width*5/13
+    const new_height = width * 5 / 13;
+    $('.carousel-img').height(new_height);
+    $('.carousel-img').css('min-height', new_height);
+    $('.carousel-img').css('max-height', new_height);
+
+    $(window).resize(function () {
+        const width = $('.carousel-img').width();
+        const new_height = width * 5 / 13;
+        $('.carousel-img').height(new_height);
+        $('.carousel-img').css('min-height', new_height);
+        $('.carousel-img').css('max-height', new_height);
+    });
 })

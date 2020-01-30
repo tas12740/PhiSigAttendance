977c33fddad3d8d6ad6e5a80c55f37609e42f3fe
tas12740 <tas12740@gmail.com>
2020-01-29 19:26:18 -0500

    Adding Allie, ICR, and NCR to Contact and Committee pages

--- static/root/js/index.js
+++ static/root/js/index.js
@@ -4,7 +4,7 @@ $(document).ready(function () {
         pause: false
     });
 
-    const width = $('.carousel-img').width();
+    const width = $(window).width();
     // 13/5 = curr_width/curr_height => curr_height = curr_width*5/13
     const new_height = width * 5 / 13;
     $('.carousel-img').height(new_height);
@@ -12,7 +12,7 @@ $(document).ready(function () {
     $('.carousel-img').css('max-height', new_height);
 
     $(window).resize(function () {
-        const width = $('.carousel-img').width();
+        const width = $(window).width();
         const new_height = width * 5 / 13;
         $('.carousel-img').height(new_height);
         $('.carousel-img').css('min-height', new_height);

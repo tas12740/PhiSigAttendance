9b9bd359ab05c08ad76a1a6a813f01193aa1f0f5
tas12740 <tas12740@gmail.com>
2020-02-07 21:00:14 -0500

    Adding link to subrosa form

--- ipanel/static/ipanel/js/index.js
+++ ipanel/static/ipanel/js/index.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
     $('#subrosa').on('click', function () {
-        window.open('http://www.google.com', "_blank");
+        window.open('https://docs.google.com/forms/d/1AIr9S72el9Jkuu9ir33M1AVq39JxvGEvcEd89mJ6N-k/viewform?fbclid=IwAR2vg_dwj8jwR7nrOdeIjeOAMxd9dur9krlMaiu--M9X7mA5eub2VbzmfEg&edit_requested=true', "_blank");
     });
 });
